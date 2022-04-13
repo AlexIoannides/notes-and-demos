fbd1fcb3b90658b67d198d3031564efd5549c411
alexioannides <alex.ioanndies@yahoo.co.uk>
2022-04-13 22:19:23 +0100

    Rerun pipelines.

--- dvc-pipelines/stages/config.py
+++ dvc-pipelines/stages/config.py
@@ -2,5 +2,5 @@
 Pipeline stage configuration.
 """
 DATASET_FILENAME = "artefacts/dataset.csv"
-METRICS_FILENAME = "artefacts/metrics.json"
+METRICS_FILENAME = "metrics/metrics.json"
 MODEL_FILENAME = "artefacts/model.joblib"
