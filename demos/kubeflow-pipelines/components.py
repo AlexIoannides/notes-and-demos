4be5f8ffa4caaacede96e5b128669c354710a248
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-02-14 15:21:23 +0000

    Add core example

--- demos/kubeflow-pipelines/components.py
+++ demos/kubeflow-pipelines/components.py
@@ -1,12 +1,51 @@
-"""Reusable pipeline components."""
-from kfp import dsl
+"""Patterrns for developing reusable KFP pipeline components."""
+import shutil
+from pathlib import Path
+from unittest.mock import Mock
+
+import numpy as  np
+from kfp import dsl, local
+
+_BASE_IMAGE = "python3.10"
+_REQUIREMENTS = Path("requirements.txt").read_text().splitlines()
 
 
-@dsl.component(base_image="python3.10", packages_to_install=["numpy==1.26.*"])
-def make_data(n_rows: int, n_cols: int, data: dsl.Output) -> None:
-    """Generate data using random number generation."""
+@dsl.component(base_image=_BASE_IMAGE, packages_to_install=_REQUIREMENTS)
+def make_data(n_rows: int, n_cols: int, data: dsl.Output[dsl.Dataset]) -> None:
+    """Synthetic dataset generation pipeline component. """
+    from numpy import save
     from numpy.random import default_rng
 
     rng = default_rng(42)
     data_arr = rng.standard_normal((n_rows, n_cols))
-    data_arr.tofile(data.path)
+    save(data.path, data_arr)
+
+
+def test_make_data_component():
+    output_dataset_file = "foo.npy"
+    mock_dataset = Mock()
+    mock_dataset.path = output_dataset_file
+    try:
+        make_data.execute(n_rows=3, n_cols=2, data=mock_dataset)
+        output_dataset = np.load(output_dataset_file)
+        assert output_dataset.shape == (3, 2)
+    except Exception:
+        assert False
+    finally:
+        data_filepath = Path(output_dataset_file)
+        if data_filepath.exists():
+            data_filepath.unlink()
+
+
+def test_make_data_component_integration():
+    kfp_root_dir = "./kfp_outputs"
+    local.init(runner=local.SubprocessRunner(use_venv=True), pipeline_root=kfp_root_dir)
+    try:
+        task = make_data(n_rows=3, n_cols=2)
+        output_dataset = np.load(f"{task.outputs['data'].path}.npy")
+        assert output_dataset.shape == (3, 2)
+    except Exception:
+        assert False
+    finally:
+        shutil.rmtree(kfp_root_dir, ignore_errors=True)
+    
